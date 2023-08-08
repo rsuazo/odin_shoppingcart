172b69c96c0907b75a81b3f816956974fd1c2c94
rsuazo <rsuazo2@gmail.com>
2023-08-07 21:32:27 -0500

    Add basic product formatting

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,14 @@
+import goldensnitch from "../assets/images/goldensnitch.webp";
+
 export default function ProductCard() {
   return (
     <>
-      <div className="product">Product Card</div>
+      <div className="product">
+        <img src={goldensnitch}></img>
+        <h1>Product Card</h1>
+        <p className="productText">This is our most famous wand!</p>
+        <button>Add to Cart</button>
+      </div>
     </>
   );
 }
