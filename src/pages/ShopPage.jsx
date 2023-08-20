0089524cd154b61fb32a9fc522d763efc7399db4
rsuazo <rsuazo2@gmail.com>
2023-08-19 19:23:19 -0500

    Add loop to render products

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -7,24 +7,16 @@ export default function ShopPage() {
     <div className="shop_container">
       <h1>Shop Home</h1>
       <div className="product_card_container">
-        <ProductCard
-          imageLink={data.wands[0].imagePath}
-          name={data.wands[0].name}
-          price={data.wands[0].price}
-          description={data.wands[0].description}
-        />
-        <ProductCard
-          imageLink={data.wands[1].imagePath}
-          name={data.wands[1].name}
-          price={data.wands[1].price}
-          description={data.wands[1].description}
-        />
-        <ProductCard
-          imageLink={data.wands[2].imagePath}
-          name={data.wands[2].name}
-          price={data.wands[2].price}
-          description={data.wands[2].description}
-        />
+        {data.map(function (product) {
+          return (
+            <ProductCard
+              imageLink={product.imagePath}
+              name={product.name}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
       </div>
     </div>
   );
